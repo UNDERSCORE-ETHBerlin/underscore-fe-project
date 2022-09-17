28f8f9aaa74c9a49acc58c62dc32f7521532ee5b
firatstory.eth <firat.tale@auto1.com>
2022-09-17 11:34:18 +0200

    create card (#2)

--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -3,4 +3,6 @@ import { addresses, abis } from "@my-app/contracts";
 import { Contract } from "@ethersproject/contracts";
 
 const factoryInferface = new utils.Interface(abis.factory);
+const listingInterface = new utils.Interface(abis.listing);
 export const factoryContract = new Contract(addresses.factory, factoryInferface);
+export const listingContract = new Contract(addresses.listing, listingInterface);
